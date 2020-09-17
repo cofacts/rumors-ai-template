2e09f6e3512ae45f2bc6c952c2a7b2bc6bff7b95
godgunman@gmail.com <godgunman@gmail.com>
2020-09-17 18:44:31 +0800

    Add mode setting

--- src/model.ts
+++ src/model.ts
@@ -1,12 +1,33 @@
 import { Model, TaskResult, createModel, getModels, getTasks, returnTasks, sleep } from './libs';
 
+const MODE = {
+  fast: {
+    defaultModel: 'fast-template',
+    sleepTime: 100,
+    isRealtime: true,
+  },
+  slow: {
+    defaultModel: 'show-template',
+    sleepTime: 10 * 1000,
+    isRealtime: false,
+  },
+};
+
 const main = async () => {
-  const defaultModel = 'template';
+  const mode = process.env.MODE || 'fast';
+  console.log(`[main] mode = ${mode}`);
+
+  if (mode !== 'fast' && mode !== 'slow') {
+    console.error(`[main] wrong mode = ${mode}`);
+    return;
+  }
+
+  const { defaultModel, sleepTime, isRealtime } = MODE[mode];
   const models = (await getModels()).filter(m => m.name === defaultModel);
 
   let myModel: Model;
   if (models.length === 0) {
-    myModel = await createModel(defaultModel, true);
+    myModel = await createModel(defaultModel, isRealtime);
   } else {
     myModel = models[0];
   }
@@ -16,6 +37,8 @@ const main = async () => {
   while (true) {
     const tasks = await getTasks(modelId);
     console.log(`[main] get task, length = ${tasks.length}`);
+
+    await sleep(sleepTime);
     const taskResults: TaskResult[] = tasks.slice(0, 50).map((t: any) => (
       {
         id: t.id,
@@ -26,7 +49,7 @@ const main = async () => {
     ));
     const result = await returnTasks(taskResults);
     console.log('[main] result = ', result);
-    await sleep(1 * 1000);
+
     if (tasks.length === 0) break;
   }
   console.log('[done]');
